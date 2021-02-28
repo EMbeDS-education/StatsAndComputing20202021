b596f68e2de1749e4e05578fe3ad3df14d74fc9e
andrea-vandin <andrea.vandin@santannapisa.it>
2021-02-28 12:01:53 +0100

    extra assignments

--- IPDP/extra_assignments/1.4. Input/print_ Previous and next/submissions/DDD LLL/main.py
+++
@@ -1,5 +0,0 @@
-# Read an integer:
-a = int(input())
-# Print a value:
-print("The next number for the number %s is %s" % (a, int(a)+2) )
-print("The previous number for the number %s is %s" % (a, int(a)-1) )
